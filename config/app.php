852b18d36eca14c46a4801df29c10f76c248ebd6
lutfifuadi <luthfifuadimajid@gmail.com>
2026-07-27 14:23:09 +0700

    chore: bump version to v1.14.2

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => env('APP_VERSION', '1.14.0'),
+    'version' => env('APP_VERSION', '1.14.2'),
     'multipurpose_mode' => env('APP_MULTIPURPOSE_MODE', 'standalone'),
 
     /*
